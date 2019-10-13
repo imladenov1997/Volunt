451e73839a569e7cd3c876fb81d0ccea8602baa2
Ivo <imladenov1997@gmail.com>
2019-10-13 16:41:05 +0300

    Basic API started

--- server/src/volunt/main.go
+++ server/src/volunt/main.go
@@ -1,11 +1,35 @@
 package main
 
 import (
-	"log",
-	"net/http",
-	"github.com/gorilla/mux"
+	"encoding/json"
+	"net/http"
 )
 
+type Args struct {
+	Arguments []string
+}
+
+type Home struct {
+	Success string
+}
+
+type APIFunc func(w http.ResponseWriter, r *http.Request)
+
+func returnJSON(f func() interface{}) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r  *http.Request) {
+		resp := f()
+		jsonObj, _ := json.Marshal(resp)
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(jsonObj)
+	}
+}
+
+func home() interface{} {
+	homeResp := Home{"Test"}
+	return homeResp
+}
+
 func main() {
-	
+	http.HandleFunc("/", returnJSON(home))
+	http.ListenAndServe(":3000", nil)
 }
